3a52c5d4771b56088b43716ac4b3f11685f6ea54
Buğra Can Sefercik <bsefercik@gmail.com>
2018-09-21 08:20:25 +0300

    updates

--- PC/App.py
+++ PC/App.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import argparse
 
 from RSSIKitFactory import RSSIKitFactory, OSType
 
@@ -9,3 +10,12 @@ kit = factory.getInstance()
 scanned = kit.scanRSSI()
 
 print(kit.collectRSSI(tag=1, count=2))
+
+
+
+if __name__ == '__main__':
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-t", "--task", type=str, default='predict')
+	ap.add_argument("-s", "--slots", type=str, default='@location_to=üsküdar,istanbul')
+	ap.add_argument("-l", "--language", type=str, default='TR')
+	opt = vars(ap.parse_args())
