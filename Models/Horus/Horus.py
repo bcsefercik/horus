c40ed66ce8e8a988395a5dcc32cbe6d9ab21aebb
Buğra Can Sefercik <bsefercik@gmail.com>
2018-10-29 06:55:07 +0300

    restructured

---
+++ Models/Horus/Horus.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+import inlo_utils as iu
+
+class Horus():
+	def __init__(self):
+		pass
+
+	def loadParseData(self, datasetPath):
+		
---
+++ inlo_utils.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+def recursiveReplace(string, search, replace):
+	if not (type(string) == str and type(search) == str and type(replace) == str):
+		return None
+
+	while search in string:
+		string = string.replace(search, replace)
+
+	return string
+	
+
+def printLog(text, debugMode=True, logType="info", title=True):
+	logTypes = ["info", "ok", "warning", "error"]
+	titles = {"info": "INFO", "ok": "SUCCESS", "warning": "WARNING", "error": "ERROR"}
+	colors = {"ok": "\033[92m", "warning": "\033[93m", "error": "\033[91m"}
+	endc = "\033[0m"
+
+	if isinstance(logType, int): 
+		if logType >= len(logTypes):
+			return -1
+
+		logType = logTypes[logType]
+	else:
+		if not logType in logTypes:
+			return -1
+
+	if title:
+		spaces = " " * (len(titles[logType]) + 2)
+		spaces = "\n" + spaces
+		text = text.replace("\n", spaces)
+		text = titles[logType] + ": " + text
+
+	if logType != "info":
+		text = colors[logType] + text + endc
+
+	if debugMode:
+		print(text)
+
+	return 0
